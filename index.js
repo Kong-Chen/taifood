9877b35bafd969273618ee953e27a36830d0d54f
kong <kong1229@gmail.com>
2019-06-12 20:43:50 +0800

    do betterV3

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //引用'http'模組
 const http = require('http');
 
+const PORT = process.env.PORT || 5000;
+//...
+
+
 //設定server網址，因為在本機端測試，所以輸入127.0.0.1
 const hostname = '127.0.0.1';
 //設定port號，這邊多少都可以，不要和現有的port重複就好
-const port = 3000;
+//const port = 3000;
 
 //新增一個server並指定他的頁面資訊，內容為'Hello World!'
 const server = http.createServer((req, res) => {
@@ -12,8 +16,8 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World!\n');
 });
-
+server.listen(PORT);
 //監聽剛剛新建的server，開啟後就執行裡面的console.log
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+//server.listen(port, hostname, () => {
+   console.log(`Server running at http://`);
+// });
